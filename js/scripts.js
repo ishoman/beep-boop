976bb7ec1d183ba7eac4303ef15076052ac480f0
Isaac Shoman <isaac.shoman@gmail.com>
2018-03-30 16:46:25 -0700

    Add business and ui logic to reverse output

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 // Business Logic
+// Main Function
 var beepBoop = function(input) {
-  debugger;
   var containsOne = (/[1]/g.test(input))
   var containsZero = (/[0]/g.test(input))
   var beepBoopResult = "";
   var countTo = parseInt(input);
   if (input % 3 === 0 && input !== 0) {
-    beepBoopResult +=  "I'm sorry, Dave. I'm afraid I can't do that."
+    beepBoopResult +=  "I'm sorry, Dave. I'm afraid I can't do that.";
   } else if (containsOne === true) {
       beepBoopResult += "Boop!";
   } else if (containsZero === true && containsOne === false) {
@@ -18,15 +18,42 @@ var beepBoop = function(input) {
     }
     return beepBoopResult;
   }
+// Function that Reverses the Output
 
-
+var beepBoopReverse = function(input) {
+  var containsOne = (/[1]/g.test(input))
+  var containsZero = (/[0]/g.test(input))
+  var beepBoopResult = "";
+  var countTo = parseInt(input);
+  if (input % 3 === 0 && input !== 0) {
+    beepBoopResult +=  "I'm sorry, Dave. I'm afraid I can't do that.";
+  } else if (containsOne === true) {
+      beepBoopResult += "Boop!";
+  } else if (containsZero === true && containsOne === false) {
+      beepBoopResult += "Beep!";
+    } else {
+      for (index=0; index <= countTo; index++) {
+        beepBoopResult = beepBoopResult + index + " ";
+      }
+    }
+    var beepBoopArray = beepBoopResult.split("").reverse();;
+    var beepBoopResultReverse = beepBoopArray.join("");
+    return beepBoopResultReverse;
+  }
 
 // User Interface Logic
 $(document).ready(function() {
   $("form#numberInput").submit(function(event){
     event.preventDefault();
-    var input = $("#input").val();
+    var input = parseInt($("#input").val());
     var output = beepBoop(input);
+    $("#result").text(output);
+  });
+
+  $("form#numberInputReverse").submit(function(event){
+    event.preventDefault();
+    var input = parseInt($("#input").val());
+    var output = beepBoopReverse(input);
     $("#result").text(output);
   });
 });
