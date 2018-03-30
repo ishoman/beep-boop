b1216b6dec173da31dc497d854847a51aa54bbaa
Isaac Shoman <isaac.shoman@gmail.com>
2018-03-30 12:07:51 -0700

    Add working front end

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 // Business Logic
 var beepBoop = function(input) {
   var inputArray = input.toString().split("");
-  console.log(inputArray);
   if (input % 3 === 0 && input !== 0) {
     return "I'm sorry, Dave. I'm afraid I can't do that."
-  } else if (input === 1) {
+  } else if (input % 3 !== 0) {
+    for (var index = 0; index < inputArray.length; index ++)
+      // var indexValue = parseInt(inputArray[i]);
+      // if
     return "Boop!";
   } else if (input === 0){
     return "Beep!";
@@ -18,8 +20,10 @@ var beepBoop = function(input) {
 
 // User Interface Logic
 $(document).ready(function() {
-  $("#numberInput").submit(function(event){
+  $("form#numberInput").submit(function(event){
     event.preventDefault();
-
+    var input = parseInt($("#input").val());
+    var output = beepBoop(input);
+    $("#result").text(output);
   });
 });
