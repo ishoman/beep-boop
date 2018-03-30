064e713da3228d9de10c86e91a30d0891ed3f966
Isaac Shoman <isaac.shoman@gmail.com>
2018-03-30 11:46:15 -0700

    Put backend if statments into proper priority order

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 // Business Logic
 var beepBoop = function(input) {
-  if (input === 0) {
-    return "Beep!";
+  // var inputArray = input.toString().split("");
+  // console.log(inputArray);
+  if (input % 3 === 0 && input !== 0) {
+    return "I'm sorry, Dave. I'm afraid I can't do that."
   } else if (input === 1) {
     return "Boop!";
-  } else if (input % 3 === 0){
-    return "I'm sorry, Dave. I'm afraid I can't do that."
+  } else if (input === 0){
+    return "Beep!";
   }
 
 };
