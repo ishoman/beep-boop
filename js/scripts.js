1584e8ee540ac61703f0d9c0eb130c40dc591087
Isaac Shoman <isaac.shoman@gmail.com>
2018-03-30 10:38:23 -0700

    Add frontend logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-ksd 
+// Business Logic
+
+
+
+
+
+
+// User Interface Logic
+$(document).ready(function( {
+  $("form#numberInput").submit(function(event){
+    var input = $("#input").val();
+    var output = beepBoop(input);
+    $("#result").text(output);
+    event.preventDefault();
+  });
+});
