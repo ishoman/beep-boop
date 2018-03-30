d18e9f73f38fffe30136819494a9dd73922b4e2f
Isaac Shoman <isaac.shoman@gmail.com>
2018-03-30 11:49:28 -0700

    Add logic for spec 4

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // Business Logic
 var beepBoop = function(input) {
-  // var inputArray = input.toString().split("");
-  // console.log(inputArray);
+  var inputArray = input.toString().split("");
+  console.log(inputArray);
   if (input % 3 === 0 && input !== 0) {
     return "I'm sorry, Dave. I'm afraid I can't do that."
   } else if (input === 1) {
