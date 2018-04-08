e3cb0621ddf34b7800d4502121dd3745b6b713c2
Isaac Shoman <isaac.shoman@gmail.com>
2018-04-08 12:21:37 -0700

    Fix for loop and add reverse output option

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,35 @@
 // Business Logic
-// Main Function
-var beepBoop = function(input) {
-  debugger;
+var beepBoop = function(input, reverse) {
   var beepBoopArray = [];
   for (var index = 0; index <= input; index++) {
     beepBoopArray.push(index);
   }
   var outputArray = [];
-  for (var index = 0; index <= beepBoopArray.length; index++) {
+  for (var index = 0; index < beepBoopArray.length; index++) {
     if (beepBoopArray[index] % 3 === 0 && beepBoopArray[index] !== 0) {
       outputArray.push(" " + "I'm sorry, Dave. I'm afraid I can't do that.");
-    } else if (beepBoopArray.includes(1)) {
+    } else if (beepBoopArray[index].toString().split("").includes("1")) {
       outputArray.push(" " + "Boop!");
-    } else if (beepBoopArray.includes(0)) {
+    } else if (beepBoopArray[index].toString().split("").includes("0")) {
       outputArray.push(" " + "Beep!");
     } else {
       outputArray.push(" " + beepBoopArray[index])
     }
+  }
+  if (reverse === "No") {
     return outputArray;
+  } else if (reverse === "Yes") {
+    return outputArray.reverse();
   }
 };
-// Function that Reverses the Output
-
-
 
 // User Interface Logic
 $(document).ready(function() {
   $("form#numberInput").submit(function(event){
     event.preventDefault();
     var input = parseInt($("#input").val());
-    var output = beepBoop(input);
-    $("#result").text(output);
-  });
-
-  $("form#numberInputReverse").submit(function(event){
-    event.preventDefault();
-    var input = parseInt($("#input").val());
-    var output = beepBoopReverse(input);
+    var reverse = $("#reverse").val();
+    var output = beepBoop(input, reverse);
     $("#result").text(output);
   });
 });
